2fe88d35fabb42b77dc15aeccb72ae9791bdb150
clairefurino88 <33135335+clairefurino88@users.noreply.github.com>
2018-04-08 18:36:55 -0400

    everything is functioning except movie-this

--- liri.js
+++ liri.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-
 var fs = require("fs");
 var request = require("request");
 var keys = require("./keys.js");
@@ -10,28 +9,26 @@ var client = new Twitter(keys.twitter);
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
-var operation = process.argv[2];
+/***************************ARGUMENTS TAKEN IN FROM THE TERMINAL**************************************/
+var command = process.argv[2];
 var songName = process.argv[3];
 var movieName = process.argv[3];
 
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-
-
-if (operation === "my-tweets") {
+/***************************CONDITIONS**************************************/
+if (command === "my-tweets") {
     my_Tweets();
 }
-else if (operation === "spotify-this-song") {
-    spotify_this_song();
+else if (command === "spotify-this-song") {
+    spotify_call();
 }
-else if (operation === "movie-this") {
-    movie_this();
+else if (command === "movie-this") {
+    movie_call();
 }
-else if (operation === "do-what-it-says") {
+else if (command === "do-what-it-says") {
     do_what_it_says();
 }
 
-
+/***************************FUNCTION FOR TWEETS**************************************/
 function my_Tweets() {
     client.get('statuses/user_timeline', { count: 20 }, function (error, tweets, response) {
         if (!error) {
@@ -48,7 +45,8 @@ function my_Tweets() {
     });
 }
 
-function spotify_this_song() {
+/***************************FUNCTION FOR SPOTIFY**************************************/
+function spotify_call() {
     if (!songName) {
         spotify.search({ type: "album,track", query: "The Sign", limit: 5 }, function (err, data) {
             if (!err) {
@@ -60,7 +58,6 @@ function spotify_this_song() {
                     }
                     console.log("External Link: ", data.albums.items[i].external_urls.spotify, "\n");
                 }
-                //console.log(data.albums.items[0]);
             }
             else {
                 return console.log('Error occurred: ' + err);
@@ -74,12 +71,10 @@ function spotify_this_song() {
                     console.log("\Song Name: ", data.albums.items[i].name);
                     console.log("Artist(s):");
                     for (var j = 0; j < data.albums.items[i].artists.length; j++) {
-                        // var artists = [];
                         console.log(data.albums.items[i].artists[j].name);
                     }
                     console.log("External Link: ", data.albums.items[i].external_urls.spotify, "\n");
                 }
-                // console.log(data.albums.items[0]);
             }
             else {
                 return console.log('Error occurred: ' + err);
@@ -88,63 +83,34 @@ function spotify_this_song() {
     }
 }
 
-function movie_this() {
-    if (!movieName) {
-
-        queryUrl = "http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy";
-
-        request(queryUrl, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                console.log("\nTitle: ", JSON.parse(body).Title);
-                console.log("Release Date: ", JSON.parse(body).Released);
-                console.log("IMDB Rating: ", JSON.parse(body).imdbRating);
-                console.log("Rotten Tomatoes Rating: ", JSON.parse(body).Ratings[1].Value);
-                console.log("Country: ", JSON.parse(body).Country);
-                console.log("Language: ", JSON.parse(body).Language);
-                console.log("Plot: ", JSON.parse(body).Plot);
-                console.log("Actors: ", JSON.parse(body).Actors, "\n");
-
-                console.log("If you haven't watched 'Mr. Nobody', then you should: http://www.imdb.com/title/tt0485947/");
-                console.log("It's on Netflix!\n");
-            }
-            else {
-                return console.log("Error Occurred: ", error);
-            }
-        });
-    }
-    else {
-        request(queryUrl, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                console.log("\nTitle: ", JSON.parse(body).Title);
-                console.log("Release Date: ", JSON.parse(body).Released);
-                console.log("IMDB Rating: ", JSON.parse(body).imdbRating);
-                if (JSON.parse(body).Ratings[1].Source === "Rotten Tomatoes")
-                {
-                    console.log("Rotten Tomatoes Rating: ", JSON.parse(body).Ratings[1].Value);
-                }
-                else{
-                    console.log("Rotten Tomatoes Rating: N/A");
-                }
-                console.log("Country: ", JSON.parse(body).Country);
-                console.log("Language: ", JSON.parse(body).Language);
-                console.log("Plot: ", JSON.parse(body).Plot);
-                console.log("Actors: ", JSON.parse(body).Actors, "\n");
-            }
-            else {
-                return console.log("Error Occurred: ", error);
-            }
-        });
-    }
+/************************************FUNCTION FOR MOVIES***************************************/
+function movie_call(movieName) {
+    request("http://www.omdbapi.com/?t=" + movieName + "&plot=short&apikey=trilogy&tomatoes=true", function(error, response, body) {
+        if(movieName === undefined) {
+            return console.log("Please enter a movie name in quotes");
+        }
+        if (!error && response.statusCode === 200) {
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Year: " + JSON.parse(body).Year);
+            console.log("IMDB Score: " + JSON.parse(body).imdbRating);
+            console.log("RT Score: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+        }
+    });
 }
 
+/*******************************FUNCTION FOR TEXT INSIDE RANDOM.TXT**************************************/
 function do_what_it_says() {
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (!error) {
             var dataArr = data.split(",");
-            operation = dataArr[0];
+            command = dataArr[0];
             songName = dataArr[1];
-            if(operation === "spotify-this-song"){
-                spotify_this_song();
+            if(command === "spotify-this-song"){
+                spotify_call();
             }
         }
         else {
